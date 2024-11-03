1b6364977e78bce5027b28cb0025b755598ef9bb
brahmaiah <30312324+bramhi123@users.noreply.github.com>
2024-11-03 08:14:08 +0530

    Create echo-addlines.sh

---
+++ echo-addlines.sh
@@ -0,0 +1,3 @@
+https://askubuntu.com/questions/968576/appending-a-line-to-a-file-in-the-cat-command
+https://stackoverflow.com/questions/11322807/echo-multiple-lines-into-a-file
+echo -e '<role rolename="admin-gui,manager-gui,manager-script,manager-jmx,manager-status"/>\n<user username="admin" password="admin123" roles="admin-gui,manager-gui,manager-script"/>' >> /opt/tomcat/conf/tomcat-users.xml
