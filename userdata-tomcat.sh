0d0c60c72910c36bccd88f0923ec680fc7df7cd3
brahmaiah <30312324+bramhi123@users.noreply.github.com>
2024-11-28 15:10:15 +0530

    Update userdata-tomcat.sh

--- userdata-tomcat.sh
+++ userdata-tomcat.sh
@@ -2,8 +2,9 @@
 sudo apt-get update -y
 sudo apt install wget -y
 #sudo apt install fontconfig openjdk-17-jre -y
-sudo apt install default-jdk -y
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.31/bin/apache-tomcat-10.1.31.tar.gz
+#sudo apt install default-jdk -y
+sudo apt install openjdk-21-jdk -y
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz
 sudo mkdir -p /opt/tomcat
 sudo tar xzvf apache-tomcat-*tar.gz -C /opt/tomcat --strip-components=1
 sudo groupadd tomcat
